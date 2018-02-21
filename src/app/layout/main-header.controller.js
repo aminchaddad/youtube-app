e3315dc441af52e88a4048dc13ce5bf537344399
Amin Chaddad <amin.chaddad@deemalab.com>
2018-02-21 08:27:52 +0200

    Add web view header and minor UI changes

--- src/app/layout/main-header.controller.js
+++ src/app/layout/main-header.controller.js
@@ -6,14 +6,33 @@
     .controller('MainHeaderController', MainHeaderController);
 
   /* @ngInject */
-  function MainHeaderController($state) {
+  function MainHeaderController($state,$rootScope) {
     var vm = this;
-    vm.searchQuery = '';
+    vm.showTextInput = false;
+    vm.searchQuery = $state.params.query?$state.params.query:'';
     vm.onKeyPress = onKeyPress;
+    vm.onButtonClick = onButtonClick;
     
     function onKeyPress(event) {
       if (event.which === 13) {
-        $state.go('search', {query: vm.searchQuery})
+        $state.go('search', {query: vm.searchQuery});
+        vm.showTextInput = false;
+      }
+    }
+
+    function onButtonClick() {
+      if ($rootScope.mobileDevice) {
+        $state.go('search', {query: vm.searchQuery});
+      }
+      else {
+        if (vm.showTextInput) {
+          $state.go('search', {query: vm.searchQuery});
+          vm.showTextInput = false;
+        }
+        else {
+          vm.searchQuery = '';
+          vm.showTextInput = true;
+        }
       }
     }
   }
